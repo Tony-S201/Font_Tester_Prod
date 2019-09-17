55c4b22f5f6052c0c3c015ec0b276ec3e9374980
Tony <swierz.tony@gmail.com>
2019-09-17 22:01:23 +0200

    add product version

---
+++ precache-manifest.31a9b836b761059f9ff74a743c897269.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6370fc3df2926f57ad23ab35e4075fa",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a881a2abe56f75baf42",
+    "url": "./static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "a4a6d1410b391d6902f6",
+    "url": "./static/css/main.2d68cb57.chunk.css"
+  },
+  {
+    "revision": "5a881a2abe56f75baf42",
+    "url": "./static/js/2.674be42d.chunk.js"
+  },
+  {
+    "revision": "a4a6d1410b391d6902f6",
+    "url": "./static/js/main.5c085dbd.chunk.js"
+  },
+  {
+    "revision": "d8c09df9a239eff7b5f9",
+    "url": "./static/js/runtime~main.65ebabd5.js"
+  }
+]);
